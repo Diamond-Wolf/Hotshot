888e7e019933095b70537d0412c941f66c202410
Diamond-Wolf <mconnorh@criticalmass.pro>
2024-10-06 17:34:42 -0600

    Clean up build notes etc

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,11 @@
 newoption {
 	trigger = "build-editor",
-	description = "Build with internal editor"
+	description = "Build with internal editor (untested)"
+}
+
+newoption {
+	trigger = "build-network",
+	description = "Build with network support (untested)"
 }
 
 newoption {
@@ -102,6 +107,10 @@ workspace "Hotshot"
 			"ui/*.cpp",
 			"ui/*.h"
 		}
+		defines "EDITOR"
+		
+	filter { "options:build-network" }
+		defines "NETWORK"
 
 	filter { "system:windows" }
 		files {
