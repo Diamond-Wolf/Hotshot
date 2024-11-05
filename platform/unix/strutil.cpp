e9c5d3f11fa89999d7436d6983fe6c65d9210209
Diamond-Wolf <mconnorh@criticalmass.pro>
2024-11-04 21:05:48 -0700

    Fix _strnicmp

--- platform/unix/strutil.cpp
+++ platform/unix/strutil.cpp
@@ -55,7 +55,8 @@ int _strnicmp(const char *s1, const char *s2, int n)
 {
 	while(*s1 && *s2 && n)	
 	{
-		if (tolower(*s1) != tolower(*s2))	return 1;
+		if (tolower(*s1) != tolower(*s2))
+			return tolower(*s1) - tolower(*s2);
 		s1++;
 		s2++;
 		n--;
